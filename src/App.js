a17b3fce0a6fef85c3d8863be0fe7e21493351f8
Seiya-ishikawa <o63.ishikawa.seiya@gmail.com>
2021-11-28 18:42:47 +0900

    'セクション3-6'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React,{useState} from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount +1)
+  const decrement2 = () => setCount(previousCount => previousCount -1)
+
+  const reset = () => setCount(previousCount => previousCount - previousCount)
+
+  const doubler = () => setCount(count * 2)
+
+  const  checkNumber = () => {
+    if(count % 3 === 0)
+      return setCount(count/3);
+    else
+      return;
+  }
+  const divide3 = () => setCount(previousCount => previousCount%3 === 0 ?previousCount/3: previousCount)
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>+1</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={doubler}>*2</button>
+      <button onClick={divide3}>when 3 devide into 3</button>
+    </>
+  );
 }
 
 export default App;
