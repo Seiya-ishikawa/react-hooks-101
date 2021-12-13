e09dcc8867c8198f009ee8c5da9b2d460178c009
Seiya-ishikawa <o63.ishikawa.seiya@gmail.com>
2021-12-14 01:04:35 +0900

    create reudcer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,27 @@
+// action = {
+//     type: 'CREATE_EVENT',
+//     title: 'Olympic in Tokyo 2020',
+//     body: 'TOKYO 2020 !'
+// }
+
+state=[]
+state=[
+    {id:1,title:'タイトル１',body:'ボディー'}
+]
+const events = (state = [], action) => {
+    switch(action.type){
+        case 'CREATE_EVENT':
+            const event = { title: action.title, body: action.body }
+            const length = state.length
+            let id = length === 0 ? 1 : state[length - 1].id + 1
+            return [...state, { id, ...event}]
+        case 'DELETE_EVENT':
+            return state
+        case 'CREATE_ALL_EVENTS':
+            return []
+        default:
+            return state
+    }
+}
+
+export default events
