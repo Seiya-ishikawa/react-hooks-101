7b5d92dc8e33a54d72edea02cd2aece79b0c9103
Seiya-ishikawa <o63.ishikawa.seiya@gmail.com>
2022-02-10 18:55:11 +0900

    add index operationlog function

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
 import EventForm from './EventForm';
 import Events from './Events';
+import OperationLogs from './OperationLogs';
 import AppContext from '../contexts/AppContext';
+import operationLogs from '../reducers/operationLogs';
 
 const App = () => {
   const initialState = {
@@ -18,6 +20,7 @@ const App = () => {
       <div className="container-fruid">
       <EventForm/>
       <Events/>
+      <OperationLogs/>
       </div>
     </AppContext.Provider>
   )
